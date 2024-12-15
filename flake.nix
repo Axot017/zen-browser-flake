5d5e3efacabc7778aac676fc0f1d983ce6082ee2
Axot017 <Axot017@users.noreply.github.com>
2024-12-15 21:28:33 +0000

    chore: Update to 1.0.2-b.2

--- flake.nix
+++ flake.nix
@@ -6,14 +6,14 @@
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      version = "1.0.2-b.1";
+      version = "1.0.2-b.2";
       downloadUrl = {
         specific.url =
           "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-specific.tar.bz2";
-        specific.sha256 = "aaa";
+        specific.sha256 = "sha256:1dv98hg48g3kg2d0hxxg5jyf8yfdn3s7smbwii26vrpbigwp3ch7";
         generic.url =
           "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-generic.tar.bz2";
-        generic.sha256 = "bbb";
+        generic.sha256 = "sha256:0wmq21z5ncwaa989iimb0kvk6y5mk67izzj78m0hy67m0h9a3n6w";
       };
 
       pkgs = import nixpkgs { inherit system; };
