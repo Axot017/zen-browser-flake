f2c362eccb6791c82896df3df9c31bb40aa99de0
Mateusz Ledwoń <mateuszledwon@duck.com>
2024-12-27 22:05:11 +0100

    fix: remove split to generic and specific

--- flake.nix
+++ flake.nix
@@ -7,14 +7,9 @@
     let
       system = "x86_64-linux";
       version = "1.0.2-b.1";
-      downloadUrl = {
-        specific.url =
-          "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-specific.tar.bz2";
-        specific.sha256 = "aaa";
-        generic.url =
-          "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-generic.tar.bz2";
-        generic.sha256 = "bbb";
-      };
+      url =
+        "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.bz2";
+      sha256 = "aaa";
 
       pkgs = import nixpkgs { inherit system; };
 
@@ -67,15 +62,14 @@
           libXScrnSaver
         ]);
 
-      mkZen = { variant }:
-        let downloadData = downloadUrl."${variant}";
-        in pkgs.stdenv.mkDerivation {
+      mkZen = { }:
+        pkgs.stdenv.mkDerivation {
           inherit version;
           pname = "zen-browser";
 
           src = builtins.fetchTarball {
-            url = downloadData.url;
-            sha256 = downloadData.sha256;
+            url = url;
+            sha256 = sha256;
           };
 
           desktopSrc = ./.;
@@ -85,42 +79,52 @@
           nativeBuildInputs =
             [ pkgs.makeWrapper pkgs.copyDesktopItems pkgs.wrapGAppsHook ];
 
-          installPhase =
-            "  mkdir -p $out/bin && cp -r $src/* $out/bin\n  install -D $desktopSrc/zen.desktop $out/share/applications/zen.desktop\n  install -D $src/browser/chrome/icons/default/default128.png $out/share/icons/hicolor/128x128/apps/zen.png\n";
+          installPhase = ''
+            mkdir -p $out/{bin,opt/zen} && cp -r $src/* $out/opt/zen
+            ln -s $out/opt/zen/zen $out/bin/zen
+
+            install -D $desktopSrc/zen.desktop $out/share/applications/zen.desktop
+
+            install -D $src/browser/chrome/icons/default/default16.png $out/share/icons/hicolor/16x16/apps/zen.png
+            install -D $src/browser/chrome/icons/default/default32.png $out/share/icons/hicolor/32x32/apps/zen.png
+            install -D $src/browser/chrome/icons/default/default48.png $out/share/icons/hicolor/48x48/apps/zen.png
+            install -D $src/browser/chrome/icons/default/default64.png $out/share/icons/hicolor/64x64/apps/zen.png
+            install -D $src/browser/chrome/icons/default/default128.png $out/share/icons/hicolor/128x128/apps/zen.png
+          '';
 
           fixupPhase = ''
-            		  chmod 755 $out/bin/*
-            		  patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/bin/zen
-            		  wrapProgram $out/bin/zen --set LD_LIBRARY_PATH "${
-                  pkgs.lib.makeLibraryPath runtimeLibs
-                }" \
-                                --set MOZ_LEGACY_PROFILES 1 --set MOZ_ALLOW_DOWNGRADE 1 --set MOZ_APP_LAUNCHER zen --prefix XDG_DATA_DIRS : "$GSETTINGS_SCHEMAS_PATH"
-            		  patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/bin/zen-bin
-            		  wrapProgram $out/bin/zen-bin --set LD_LIBRARY_PATH "${
-                  pkgs.lib.makeLibraryPath runtimeLibs
-                }" \
-                                --set MOZ_LEGACY_PROFILES 1 --set MOZ_ALLOW_DOWNGRADE 1 --set MOZ_APP_LAUNCHER zen --prefix XDG_DATA_DIRS : "$GSETTINGS_SCHEMAS_PATH"
-            		  patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/bin/glxtest
-            		  wrapProgram $out/bin/glxtest --set LD_LIBRARY_PATH "${
-                  pkgs.lib.makeLibraryPath runtimeLibs
-                }"
-            		  patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/bin/updater
-            		  wrapProgram $out/bin/updater --set LD_LIBRARY_PATH "${
-                  pkgs.lib.makeLibraryPath runtimeLibs
-                }"
-            		  patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/bin/vaapitest
-            		  wrapProgram $out/bin/vaapitest --set LD_LIBRARY_PATH "${
-                  pkgs.lib.makeLibraryPath runtimeLibs
-                }"
-            		'';
+            chmod 755 $out/bin/zen $out/opt/zen/*
+
+            patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/opt/zen/zen
+            wrapProgram $out/opt/zen/zen --set LD_LIBRARY_PATH "${
+              pkgs.lib.makeLibraryPath runtimeLibs
+            }" \
+                                 --set MOZ_LEGACY_PROFILES 1 --set MOZ_ALLOW_DOWNGRADE 1 --set MOZ_APP_LAUNCHER zen --prefix XDG_DATA_DIRS : "$GSETTINGS_SCHEMAS_PATH"
+
+            patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/opt/zen/zen-bin
+                 wrapProgram $out/opt/zen/zen-bin --set LD_LIBRARY_PATH "${
+                   pkgs.lib.makeLibraryPath runtimeLibs
+                 }" \
+                                 --set MOZ_LEGACY_PROFILES 1 --set MOZ_ALLOW_DOWNGRADE 1 --set MOZ_APP_LAUNCHER zen --prefix XDG_DATA_DIRS : "$GSETTINGS_SCHEMAS_PATH"
+
+            patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/opt/zen/glxtest
+                 wrapProgram $out/opt/zen/glxtest --set LD_LIBRARY_PATH "${
+                   pkgs.lib.makeLibraryPath runtimeLibs
+                 }"
+
+            patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/opt/zen/updater
+                 wrapProgram $out/opt/zen/updater --set LD_LIBRARY_PATH "${
+                   pkgs.lib.makeLibraryPath runtimeLibs
+                 }"
+
+            patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/opt/zen/vaapitest
+                 wrapProgram $out/opt/zen/vaapitest --set LD_LIBRARY_PATH "${
+                   pkgs.lib.makeLibraryPath runtimeLibs
+                 }"
+          '';
 
           meta.mainProgram = "zen";
         };
-    in {
-      packages."${system}" = {
-        generic = mkZen { variant = "generic"; };
-        specific = mkZen { variant = "specific"; };
-        default = self.packages."${system}".specific;
-      };
-    };
+
+    in { packages."${system}".default = mkZen { }; };
 }
