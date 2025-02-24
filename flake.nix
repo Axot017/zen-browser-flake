73deca7017950107308c531d8be7fbd883ea278a
Axot017 <36015434+Axot017@users.noreply.github.com>
2025-02-24 06:14:57 +0000

    chore: Update to 1.8b

--- flake.nix
+++ flake.nix
@@ -6,10 +6,10 @@
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      version = "1.7.6b";
+      version = "1.8b";
       url =
         "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.xz";
-      sha256 = "sha256:19v6n0a1j63i8i7c9615lh1fmbz8jakwyiy11imc9vbq1n6z0nm9";
+      sha256 = "sha256:0x034npapnyndflhaiqk8s0wldggs4hlwhnsllq6q4vgcq2l2mwi";
 
       pkgs = import nixpkgs { inherit system; };
 
