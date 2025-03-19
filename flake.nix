77afb8e2ee13db8891322061778595b59cc749d1
Axot017 <36015434+Axot017@users.noreply.github.com>
2025-03-19 07:14:20 +0000

    chore: Update to 1.10b

--- flake.nix
+++ flake.nix
@@ -6,10 +6,10 @@
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      version = "1.9.1b";
+      version = "1.10b";
       url =
         "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.xz";
-      sha256 = "sha256:073lw306rffhr8nz8psdmdrc0vb2bmz6j02i5iabv0dyc190nk9z";
+      sha256 = "sha256:06v8caplc3qakqc9ifyfr0zmzpg83m86kc8yy8yaln77hxvw7lbz";
 
       pkgs = import nixpkgs { inherit system; };
 
