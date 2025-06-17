332b57e106650e163ae639d5caa755ec4ddfdbb6
Axot017 <36015434+Axot017@users.noreply.github.com>
2025-06-17 09:14:34 +0000

    chore: Update to 1.13.2b

--- flake.nix
+++ flake.nix
@@ -6,10 +6,10 @@
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      version = "1.13.1b";
+      version = "1.13.2b";
       url =
         "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.xz";
-      sha256 = "sha256:16zv2zhlakkfxczrhsch6972139mwjg83vb25h0dwfi9f28cqcwq";
+      sha256 = "sha256:0hmb3zxjn961nd6c0ry5mbcr2iq38i1rvqs31qg99c7mcsv6zjal";
 
       pkgs = import nixpkgs { inherit system; };
 
