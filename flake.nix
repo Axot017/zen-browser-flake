dc20febb9fbf2013db2d24955731f788849cdf35
Axot017 <36015434+Axot017@users.noreply.github.com>
2025-07-27 13:21:02 +0000

    chore: Update to 1.14.9b

--- flake.nix
+++ flake.nix
@@ -6,10 +6,10 @@
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      version = "1.14.8b";
+      version = "1.14.9b";
       url =
         "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.xz";
-      sha256 = "sha256:10a1skdfl569zmyiy8191gvcys6hj7nnzv2sq9y6laic77s9y8nd";
+      sha256 = "sha256:1k27vl9b2y87skwy1mg955n74x504dvz7w3r2yxir6ib2srnslzi";
 
       pkgs = import nixpkgs { inherit system; };
 
